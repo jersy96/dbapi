0ecec18e008b302a31d32a088f048abd18e65495
Yasir guzman <jasirg@uninorte.edu.co>
2018-10-11 10:27:59 -0500

    Se crea el controlador de User

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:update]
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :ok
+    else
+      render json: user.errors.messages, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @user.attributes = user_params
+    if @user.save
+      render json: @user, status: :ok
+    else
+      render json: @user.errors.messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.permit(
+      :name,
+      :lastname,
+      :email,
+      :password,
+      :address
+    )
+  end
+end

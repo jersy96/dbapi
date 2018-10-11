477b27b7d87eac5f30a63a23799149d70486ca7f
Javier Roncallo <jroncallo96@gmail.com>
2018-10-11 14:38:40 -0500

    Se crea el metodo if_record_not_found en ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   rescue_from Exceptions::ApiKeyNotFound, with: :if_api_key_not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :if_record_not_found
 
   def set_api_key_user
     api_key = ApiKey.find_by(key: params[:api_key])
@@ -9,5 +10,9 @@ class ApplicationController < ActionController::API
   private
   def if_api_key_not_found
     render json: {api_key: 'not found'}
+  end
+
+  def if_record_not_found(exception)
+    render json: {errors: exception.message}, status: :unprocessable_entity
   end
 end

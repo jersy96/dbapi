4b2c13ce73dc4f0a14b31daa0a369178e86410a8
Javier Roncallo <jroncallo96@gmail.com>
2018-10-11 11:25:06 -0500

    Se crea el metodo set_api_key_user en ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  rescue_from Exceptions::ApiKeyNotFound, with: :if_api_key_not_found
+
+  def set_api_key_user
+    api_key = ApiKey.find_by(key: params[:api_key])
+    raise Exceptions::ApiKeyNotFound unless api_key
+  end
+
+  private
+  def if_api_key_not_found
+    render json: {api_key: 'not found'}
+  end
 end

63b31eca600baf0cfdce99cb18a6dbc2e172e9b9
Javier Roncallo <jroncallo96@gmail.com>
2018-10-11 14:23:31 -0500

    Se crea el callback before_validate que llama al metodo set_room_id en el modelo Reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,6 +2,10 @@ class Reservation < ApplicationRecord
   # Relationships
   belongs_to :room
   belongs_to :user
+  belongs_to :hotel
+
+  # Callbacks
+  before_validation :set_room_id
 
   # Validations
   validates :start_date, :final_date, presence: true
@@ -11,5 +15,10 @@ class Reservation < ApplicationRecord
   private
   def start_date_before_final_date
     errors.add(:dates, 'final date must be after the start date') if self.final_date < self.start_date
+  end
+
+  def set_room_id
+    h = self.hotel
+    self.room_id = h.available_room(self.start_date, self.final_date) if h
   end
 end

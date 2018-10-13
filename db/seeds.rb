b75b628a32dad8647f794e1840cb05833104c9f8
Javier Roncallo <jroncallo96@gmail.com>
2018-10-13 16:15:05 -0500

    Se agrega codigo al seeds para que se lean los hoteles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+File.open("hotels.csv", "r") do |f|
+  f.each_line do |line|
+    camps = line.split(';')
+    h = Hotel.create(
+      name: camps[0], address: camps[1], state: camps[2],
+      phone: camps[3], fax: camps[4], email: camps[5],
+      website: camps[6], hotel_type: camps[7], number_of_rooms: camps[8]
+    )
+    puts "created hotel #{h.name} with email #{h.email} and id #{h.id}"
+  end
+end
